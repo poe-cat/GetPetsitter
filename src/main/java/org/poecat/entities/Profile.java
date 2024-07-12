21e6f397be614b92aae0820cf6b11e57ab90e8b2
Katarzyna Szewiel <84228264+poe-cat@users.noreply.github.com>
2024-07-12 10:36:20 +0200

    add basic Profile entity

---
+++ src/main/java/org/poecat/entities/Profile.java
@@ -0,0 +1,43 @@
+package org.poecat.entities;
+
+import java.util.List;
+
+public class Profile {
+
+    private String userId;
+    private String name;
+    private String description;
+    private List<String> reviews;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<String> reviews) {
+        this.reviews = reviews;
+    }
+}
